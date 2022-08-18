ccf50c48dada06f6002cfd44dcf11e87406b083e
JerryHuang <905056897@qq.com>
2022-08-18 17:33:35 +0800

    通过vue-cli配置服务器代理，解决跨域问题

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,7 @@
 </template>
 
 <script>
-import { onMounted } from "vue";
 import { useRoute } from "vue-router";//vue3取消了this，可以用setup的参数context获取$的一些对象($route等)
-import { reqAddress } from "./api/index";
 //但是更推荐引入函数在setup里使用
 import FootGuide from './components/FootGuide.vue';
 import HeaderTop from './components/HeaderTop.vue';
@@ -23,13 +21,9 @@ export default {
 
 setup() {
   const route = useRoute();
-  const address = reqAddress('40.10038,116.36867');
-  onMounted(() => {
-    console.log(address);
-  });
+
   return {
     route,
-    address
   };
   
 }
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave:false,
+  lintOnSave: false,
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
+  }
 })
